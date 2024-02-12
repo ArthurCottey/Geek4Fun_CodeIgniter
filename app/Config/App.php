5aa5a5c4f3aa09a57ee6796c5b4ecea9d4542490
Arthur COTTEY <112547371+ArthurCottey@users.noreply.github.com>
2024-02-12 12:47:43 +0100

    Edit App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8888/Geek4Fun_CodeIgniter';
+    public $baseURL = 'http://localhost/Geek4Fun_CodeIgniter';
 
     /**
      * --------------------------------------------------------------------------
